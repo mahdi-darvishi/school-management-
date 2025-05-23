03c63cb0806024f4b5c663224c467b51050f6e8a
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-23 12:37:47 +0330

    Modifying the name of the BigCalendar component to BigCalender and improving the design of the parent, student, and teacher pages with minor changes to spacing and section titles.

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,19 +1,19 @@
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalendar";
+import BigCalendar from "@/components/BigCalender";
 import EventCalendar from "@/components/EventCalendar";
 
 const StudentPage = () => {
   return (
-    <div className="p-4 flex flex-col xl:flex-row gap-4">
-      {/* LEft */}
-      <div className=" w-full xl:w-2/3">
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold ">Schedule (4A)</h1>
-          <BigCalendar />
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar/>
         </div>
       </div>
-      {/* Right */}
-      <div className=" w-full xl:w-1/3 flex flex-col gap-8 ">
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <EventCalendar />
         <Announcements />
       </div>
