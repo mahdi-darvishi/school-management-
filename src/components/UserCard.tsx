19be3ed3bd83b7a6b18268053f30dbb94a38e0f8
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-23 12:35:05 +0330

    Modified the order of classes in the UserCard component to improve design and layout.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const UserCard = ({ type }: { type: string }) => {
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
-      <div className="flex items-center justify-between">
+      <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
           2024/25
         </span>
