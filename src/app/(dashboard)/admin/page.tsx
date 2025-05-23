5a03be132cd260b8cd6f211cbeb892072c2cfddb
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-23 12:37:21 +0330

    Updated the design of the admin page with revised section titles and improved code readability. Also, minor changes were made to spacing and element order.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -8,37 +8,35 @@ import UserCard from "@/components/UserCard";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* Left */}
-      <div className=" w-full lg:w-2/3 flex flex-col gap-8">
-        {/* USER CARTS */}
-
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
-
-        {/* Middle Charts */}
+        {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* Count Chart */}
+          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
           </div>
-          {/* Attendance Chart */}
+          {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChart />
           </div>
         </div>
-        {/* Bottom Charts */}
+        {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
           <FinanceChart />
         </div>
       </div>
-      {/* Right */}
-      <div className=" w-full lg:w-1/3 flex flex-col gap-8 ">
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
-        <Announcements />
+        <Announcements/>
       </div>
     </div>
   );
